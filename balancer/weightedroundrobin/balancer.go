ed2bbf4e0e23e1012e87688fe3541d412870afda
Sergey Matyukevich <s.matyukevich@gmail.com>
2023-09-25 13:40:57 -0600

    add more logging

--- balancer/weightedroundrobin/balancer.go
+++ balancer/weightedroundrobin/balancer.go
@@ -488,7 +488,8 @@ func (w *weightedSubConn) OnLoadReport(load *v3orcapb.OrcaLoadReport) {
 	w.lastUtilization = utilization
 	if w.logger.V(2) {
 		w.logger.Infof("New weight for subchannel %v: %v", w.SubConn, w.weightVal)
-		w.logger.Errorf("PID state %+v", w.pidController.State)
+		w.logger.Infof("PID state %+v", w.pidController.State)
+		w.logger.Infof("Mean utilization %v", meanUtilization)
 	}
 
 	w.pidController.DischargeIntegral(time.Since(w.lastUpdated))
