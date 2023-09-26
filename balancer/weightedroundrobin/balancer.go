4025437b197f2fe6d5cab1a5e1e7c7f9a821c15a
Sergey Matyukevich <s.matyukevich@gmail.com>
2023-09-26 17:21:45 -0600

    tune p gain

--- balancer/weightedroundrobin/balancer.go
+++ balancer/weightedroundrobin/balancer.go
@@ -179,7 +179,7 @@ func (b *wrrBalancer) updateAddresses(addrs []resolver.Address) {
 				cfg: &lbConfig{EnableOOBLoadReport: false},
 				pidController: &pid.AntiWindupController{
 					Config: pid.AntiWindupControllerConfig{
-						ProportionalGain:              1,
+						ProportionalGain:              10,
 						IntegralGain:                  0,
 						DerivativeGain:                0,
 						AntiWindUpGain:                0,
